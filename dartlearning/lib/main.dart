5f275736c17af30b42f40d110cbf1ac26b513375
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-04-30 15:28:12 -0500

    feat: learned variables and functions in dart

--- dartlearning/lib/main.dart
+++ dartlearning/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+//const age = 20; const example
+// var name = "foo"; var example
+// final name = "arthur"; final make that there are not more changes
+String getFullName(String firstName, String lastName){
+// return firstName + " " + lastName;//first way
+return "$firstName $lastName";//second way
 
+}//the function return a string
+// todo 1,
+// String getFullName(String firstName, String lastName) => return "$firstName $lastName";//second form of function for short instructions
 void main() {
   runApp(const MainApp());
 }
@@ -9,6 +18,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(getFullName("foo","Bar"));//1)prove that print in console "foo bar"
     return const MaterialApp(
       home: Scaffold(
         body: Center(
