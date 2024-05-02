5d46fe668bbe84f4e8463d2a3d0ecf40077af888
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-05-02 13:34:04 -0500

    feat: Add if conditional

--- dartlearning/lib/main.dart
+++ dartlearning/lib/main.dart
@@ -7,18 +7,36 @@ String getFullName(String firstName, String lastName){
 return "$firstName $lastName";//second way
 
 }//the function return a string
-// todo 1,
+String printMyName(){
+  return "";
+}
+// todo
 // String getFullName(String firstName, String lastName) => return "$firstName $lastName";//second form of function for short instructions
+
+void test(){
+  //if conditional
+  final name = "bar";
+  if(name == "foo"){
+    print("Yes,it's foo");
+  }else if(name == "bar"){
+    print("Yes,it's bar");
+  }else{
+    print("No,it's not foo or bar");
+  }
+}
+
 void main() {
   runApp(const MainApp());
 }
 
+
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    print(getFullName("foo","Bar"));//1)prove that print in console "foo bar"
+    test();
     return const MaterialApp(
       home: Scaffold(
         body: Center(
