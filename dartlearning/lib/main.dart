e4dadb0aa0017df84ec10d24ec838df8229d29dc
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-05-19 13:21:30 -0500

    feat:end dart basics

--- dartlearning/lib/main.dart
+++ dartlearning/lib/main.dart
@@ -10,7 +10,7 @@ return "$firstName $lastName";//second way
 String printMyName(){
   return "";
 }
-// todo 1), 2 , 3 , 4, 5, 6, 7, 8, 9,10,11 and 12 
+// todo
 // String getFullName(String firstName, String lastName) => return "$firstName $lastName";//second form of function for short instructions
 
 void test(){
@@ -29,44 +29,44 @@ void test(){
   final doubleAge = age *2;
   final ageMinusOne = --age;//prefix operator -- make sure that age is subtract one first
   final nameTimes5 = name * 5;
-  print(halfOfAge + " " + doubleAge + nameTimes5);//1) test if works
+  print('$halfOfAge  $doubleAge $nameTimes5');
   //list
   var names = ["Foo","Bar","Baz"];
   final foo = name[0];
   final length = name.length;
-  print(foo + length);//2) test if works
+  print('$foo  $length');
   names.add("My name");
-  print(name.length);//3) test if works
+  print(name.length);
   //sets
   var names2= {"foo","bar","baz","foo"};
   names2.add("foo");
   names2.add("bar");
   names2.add("baz");
   const things = {"foo",1};
-  print(things);//5) personal expriment,test if works
-  print(names2)//4) test that nothing repeat
+  print(things);
+  print(names2);
   //maps
-  const person = {
+  var person = {
     "age":20,
     "name":"foo",
     };
-  print(person);//6) Test if works
+  print(person);
   person["name"] = "Foooo";
-  print(person);//7) Test if changes
+  print(person);
   person["lastname"] = "baz";
-  print(person);//8) Test if changes
+  print(person);
 
   //null
-  const String? name3 = null;// the "?" make it possible to have null
-  print(name3);//9) Test if works
-  name = "foo";
-  print(name3);//10) Test if works
-  list<String?>? names4 = ["foo","bar"];// make posible that the variables are null and that the list is absent
+  String? name3 = null;// the "?" make it possible to have null
+  print(name3);
+  name3 = "foo";
+  print(name3);
+  List<String?>? names4 = ["foo","bar"];// make posible that the variables are null and that the list is absent
   //cherry picking non-null values
   const String? firstName2 = null;
   const String? lastName2 = "bar";
   const String? middleName = "baz";
-  if(firstName2 != null){//11) prove the first way to get the not null value
+  if(firstName2 != null){
     print("First name is the first non-null value");
   } else if(middleName != null ){
     print("Middle name is the first non-null value");
@@ -78,11 +78,14 @@ void test(){
   //null awaere assignment operator "??="
   String? nameT = firstName2;
   nameT??= middleName;//if  nameT is nulll it take middle name value
-  nameT ??= lastName2;
-  print(nameT);//12)prove that this works
+  nameT??= lastName2;
+  print(nameT);
 
+// conditional invocation
+List<String>? namesCI = null;
+final lengthCI = namesCI ?.length ?? 0; // if namesCI is null, length is 0
 
-
+if(namesCI != null){final numberOfNames = names.length;}
 }
 
 void main() {
